c5ea1ff6625730a1f78e9cf82dc0f5b1507109cc
xuxiaobin <xuxiaobin@iquanwai.com>
2021-03-18 15:20:13 +0800

    第一次提交

---
+++ src/main/java/com/xiaoquan/JacocoTest.java
@@ -0,0 +1,18 @@
+package com.xiaoquan;
+
+/**
+ * @Author: 许小斌
+ * @Version 创建时间: 2021/3/18 2:39 下午
+ */
+public class JacocoTest {
+
+    public void my1() {
+
+        System.out.println("覆盖率测试1");
+    }
+
+    public void my2() {
+
+        System.out.println("覆盖率测试2");
+    }
+}
